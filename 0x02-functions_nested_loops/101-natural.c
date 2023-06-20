6e77d621cf7d035fee8a409f438038b1b89a7f28
Peter McMillan <petermcmillan25@gmail.com>
2023-06-20 06:14:51 -0700

    All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,22 @@
-#include"main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
  *
- * Description: computes the sum of all
- *		the multiples of 3 or 5
- *		below 1024 (excluded)
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int sum, num;
+        int i, sum = 0;
 
-	for (num = 0; num < 1024; ++num)
-	{
-		if ((num % 3 == 0) || (num % 5 == 0))
-			sum += num;
-	}
-	printf("%d\n", sum);
+        for (i = 0; i < 1024; i++)
+        {
+                if ((i % 3) == 0 || (i % 5) == 0)
+                        sum += i;
+        }
 
-	return (0);
+        printf("%d\n", sum);
+
+        return (0);
 }
