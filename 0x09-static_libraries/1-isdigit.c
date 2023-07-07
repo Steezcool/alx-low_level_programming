b0a8090d50c50c8b72918a9b24b77b7ac47fbce1
Peter McMillan <petermcmillan25@gmail.com>
2023-07-07 02:10:44 -0700

    C - Static libraries all being pushed at once

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is an upper case letter or not
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+
+}
