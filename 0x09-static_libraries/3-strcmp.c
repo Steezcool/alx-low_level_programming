3b4e4783e79f4ce83c308ba30ec201d2516e0471
Peter McMillan <petermcmillan25@gmail.com>
2023-07-07 03:40:24 -0700

    third update

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -4,8 +4,8 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if s1 and s2 are equal,
- * and another number if not.
+ * Return: 0 if s1 and s2 are equal
+ * and another number if not
  */
 
 int _strcmp(char *s1, char *s2)
