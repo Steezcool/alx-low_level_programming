6305b9085f6892fee3c8ab1a5dbfcbf09830b157
Peter McMillan <petermcmillan25@gmail.com>
2023-07-13 03:23:54 -0700

    We must accept finite disappointment but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,22 +1,10 @@
-#ifndef main_h
-#define main_h
-
-/**
- * Main - main.h
- *
- * Description: Header file containing the prototypes
- * for all of the functions for project 0x0C. C - malloc, free.
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/* PROTOTYPES */
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
